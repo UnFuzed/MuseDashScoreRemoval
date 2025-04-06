2af58f3f280fe160cc83d3d75e510068f4b32e17
UnFuzed <Unfuzedyt@gmail.com>
2025-04-06 14:37:49 -0600

    Added files

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace MuseDashScoreRemoval
+{
+    public class Main : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName != "UISystem_PC" && sceneName != "GameMain") return;
+
+            List<string> objsToRemove = new() { "TxtValue", "GradeScale", "TxtNewBest"};
+
+            List<GameObject> allObjects = Resources.FindObjectsOfTypeAll<GameObject>().ToList();
+            foreach (GameObject obj in allObjects)
+            {
+                if (objsToRemove.Contains(obj.name))
+                    obj.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+using Main = MuseDashScoreRemoval.Main;
+
+[assembly: MelonInfo(typeof(Main), "MuseDashScoreRemoval", "1.0.0", "UnFuzed")]
+[assembly: MelonGame("PeroPeroGames", "MuseDash")]
+
+[assembly: AssemblyTitle("MuseDashScoreRemoval")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Muse Dash Modding Community")]
+[assembly: AssemblyProduct("MuseDashScoreRemoval")]
+[assembly: AssemblyCopyright("Copyright © UnFuzed 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
